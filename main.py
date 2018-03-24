a9809ef169469d64a891a0a0648349879b25ddb2
fengyemuse <457838738@qq.com>
2018-03-25 07:48:51 +0800

    Fashion_ai

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ if not model_exist:
     model = model_class.create_model(base_model='VGG16')
     history = model_class.train_model(model=model,
                                       is_augumente=True,
-                                      is_image_processed=False)
-    # 对图像进行处理后，记得把is_image_processed设置成False
+                                      is_image_processed=True)
+    # 对图像进行处理后，记得把is_image_processed设置成True
     model_class.train_validation_result_plot(history)
 else:
     model = model_class.model_load()
